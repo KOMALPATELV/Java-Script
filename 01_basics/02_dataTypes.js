767d72defbd19dc5efb70150e9531b3ff3208c60
KOMALPATELV <komaldholariya531@gmail.com>
2024-05-29 09:24:04 +0000

    datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,6 +1,34 @@
-"use Strict";
+"use Strict"; // treat all JS code as newer version
 
-//alert( 3 + 3)
-console.log(3 + 3)
+//alert( 3 + 3) // we are using nodejs, not browser
 
-console.log("Komal");
+console.log(3 
+    +
+       3) // code readibility should be high
+
+console.log("Komal")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
+
+
+
+
